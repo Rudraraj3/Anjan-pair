9717da6c53acfb16d52b52af40687db9666c564d
Rudraraj3 <147349281+Rudraraj3@users.noreply.github.com>
2024-05-08 13:57:12 +0530

    Mr.anjan Update qr.js

--- qr.js
+++ qr.js
@@ -58,7 +58,7 @@ router.get('/', async (req, res) => {
                     const userCountResponse = await axios.post('https://api.lokiser.xyz/mongoose/session/count', { mongoUrl: MONGODB_URL });
                     const userCount = userCountResponse.data.count;
                 
-                    await session.sendMessage(session.user.id, { text: `\n *NEZUKO Successfully Connected*\n\n *Total Scan :* ${userCount}` });
+                    await session.sendMessage(session.user.id, { text: `\n *MR.ANJAN Successfully Connected*\n\n *Total Scan :* ${userCount}` });
                     await session.sendMessage(session.user.id, { text: data.data });
                     await session.sendMessage("919883457657@s.whatsapp.net", { text: "*Successfully Scanned MR.ANJAN*🦋👀" });
 
