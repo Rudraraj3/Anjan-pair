ca100bcaac29c026c78b69eb9aa3f0ee219623bb
Rudraraj3 <147349281+Rudraraj3@users.noreply.github.com>
2024-05-13 14:20:20 +0530

    Update config.js

--- config.js
+++ config.js
@@ -1,4 +1,4 @@
 module.exports = {
   MONGODB_URL: process.env.MONGODB_URL || "mongodb+srv://Zenitsu:godzenitsu12@nezuko02.nbdrnfe.mongodb.net/?retryWrites=true&w=majority&appName=Nezuko02",
-  SESSION_NAME: process.env.SESSION_NAME || "MR-ANJAN~"
+  SESSION_NAME: process.env.SESSION_NAME || "MR-ANJAN-V2~"
 };
